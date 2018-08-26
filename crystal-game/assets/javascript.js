58228dcf3876b4f4ab61abd832057f554a9ae249
Nick <nicholas@honyak.com>
2018-08-25 23:31:48 -0400

    add new version

--- crystal-game/assets/javascript.js
+++ crystal-game/assets/javascript.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
 
-	// ******* NUMBER ARRAYS *******
-
-	// random computer variable array
 	var rand = [];
 
 	for (var r = 19; r < 121; r++) {
 		rand.push(r);
 	}
 
-	// crystal numbers array
 	var crystals = [];
 
 	for (var i = 1; i < 13; i++) {
@@ -17,26 +13,21 @@ $(document).ready(function() {
 		crystals.push(i);
 	}
 
-	// console.log(crystals);
-
-	// ******* GLOBAL VARIABLES *******
-
-	// random variables selected by computer
-	var randNumber; // number to match
-	var crystalNumbers = []; // for array of random crystal values
+	var randNumber; 
+	var crystalNumbers = []; 
 
 	var c1;
 	var c2;
 	var c3;
 	var c4;
 
-  var totalScore = 0; // user's score
+  var userScore = 0; 
 
 	var wins = 0;
 	var losses = 0;
 
 
-	function pickRandomNumber(arr) {
+	function randNumber(arr) {
 
 		var x = arr[Math.floor(Math.random() * arr.length)];
 		randNumber = x;
@@ -45,7 +36,7 @@ $(document).ready(function() {
     }
     
 
-	function pickRandomCrystals(arr) {
+	function randomCrystal(arr) {
 
 		for (var y = 0; y < 4; y++){
 
@@ -56,82 +47,81 @@ $(document).ready(function() {
 
 	}
 
-	function crystalValues(arr) {
+	function crystalNumbers(arr) {
 		for (i = 0; i < arr.length; i++) {
 
 		$("#btn-" + (i+1)).attr("value", arr[i]);
 		}
-		c1 = arr[0];
-		c2 = arr[1];
-		c3 = arr[2];
-		c4 = arr[3];
-	} // END of crystalValues function
+		i1 = arr[0];
+		i2 = arr[1];
+		i3 = arr[2];
+		i4 = arr[3];
+	}
 
 	function gameReset(x) {
 
 		crystalNumbers = [];
 
-		pickRandomNumber(rand);
+		randNumber(rand);
 
-		pickRandomCrystals(crystals);
+		randomCrystal(crystals);
 
 		crystalValues(crystalNumbers);
 
-		totalScore = 0;
-		$("#totalNumber").html(totalScore);
+		Total = 0;
+		$("#Total").html(userScore);
 
 		alert(x);
-	} // END of gameReset function
+	} 
 
-	// *** GAME SETTINGS AT START ***
 
-	pickRandomNumber(rand); // random number to match
-	pickRandomCrystals(crystals); // array of random crystal values
-	crystalValues(crystalNumbers);
+	randNumber(rand); 
+	randomCrystal(crystals);
+	crystalNumbers(crystalNumbers);
 
 		$("#btn-1").on("click", function() {
 
-			totalScore += c1;
-			$("#totalNumber").html(totalScore);
+			Total += c1;
+			$("#Total").html(userScore);
 		});
 
 		$("#btn-2").on("click", function() {
 
-			totalScore += c2;
-			$("#totalNumber").html(totalScore);
+			Total += c2;
+			$("#Total").html(userScore);
 		});
 
 		$("#btn-3").on("click", function() {
 
-			totalScore += c3;
-			$("#totalNumber").html(totalScore);
+			Total += c3;
+			$("#Total").html(userScore);
 		});
 
 		$("#btn-4").on("click", function() {
 
-			totalScore += c4;
-			$("#totalNumber").html(totalScore);
+			Total += c4;
+			$("#Total").html(userScore);
 		});
 
 	$("button").on("click", function() {
-		// this is what happens if the user wins
-		if (totalScore == randNumber) {
+
+		if (Total == randNumber) {
 
 			wins++;
-			$("#totalNumber").html(totalScore);
+			$("#Total").html(userScore);
 			$("#wins").html("Wins: " + wins);
 
 
-			setTimeout(function() {gameReset("YOU WIN!!")}, 200);
+			setTimeout(function() {gameReset("You win!")}, 200);
 		}
 
-		else if (totalScore > randNumber){
+		else if (Total > randNumber){
 
 			losses++;
-			$("#totalNumber").html(totalScore);
+			$("#Total").html(userScore);
 			$("#losses").html("Losses: " + losses);
 
-			setTimeout(function() {gameReset("WOMP-WOMP...YOU LOSE!")}, 200);
+			setTimeout(function() {gameReset("You lose!")}, 200);
 		}
 	});
 
